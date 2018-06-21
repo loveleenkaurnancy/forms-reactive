f42fed442787b6e8c597f19e6eabc541ed03a482
loveleen <loveleen.nancy@gmail.com>
2018-06-22 00:13:28 +0530

    Final Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormControlName, Validators, FormArray, FormBuilder } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
 
 @Component({
@@ -10,27 +10,47 @@ import { Observable } from 'rxjs/Observable';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
-  forbiddenUsernames = ['Naman', 'Aman'];
+  forbiddenUsernames = ['Chris', 'Anna'];
 
-  constructor(private formBuilder:FormBuilder) {}
+  constructor() {}
 
   ngOnInit() {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email])
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails)
       }),
-      'gender': new FormControl('female'),
-      'hobbies' : new FormArray([])
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([])
+    });
+    // this.signupForm.valueChanges.subscribe(
+    //   (value) => console.log(value)
+    // );
+    this.signupForm.statusChanges.subscribe(
+      (status) => console.log(status)
+    );
+    this.signupForm.setValue({
+      'userData': {
+        'username': 'Max',
+        'email': 'max@test.com'
+      },
+      'gender': 'male',
+      'hobbies': []
+    });
+    this.signupForm.patchValue({
+      'userData': {
+        'username': 'Anna',
+      }
     });
   }
 
-  onSumbit() {
+  onSubmit() {
     console.log(this.signupForm);
+    this.signupForm.reset();
   }
 
   onAddHobby() {
-    const control=new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]);
+    const control = new FormControl(null, Validators.required);
     (<FormArray>this.signupForm.get('hobbies')).push(control);
   }
 
